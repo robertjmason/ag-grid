2bce85d0b17342a745b5f17ccfb698d4f6b66e37
Mark Durrant <mark.j.durrant@gmail.com>
2023-04-20 16:35:51 +0100

    AG-8094 - Update docs home icons

--- grid-packages/ag-grid-docs/documentation/src/components/Icon.tsx
+++ grid-packages/ag-grid-docs/documentation/src/components/Icon.tsx
@@ -5,6 +5,9 @@ import BoldChevronRight from '../images/inline-svgs/bold-chevron-right.svg';
 import EnterpriseIcon from '../images/inline-svgs/enterprise.svg';
 import ReplayDemoIcon from '../images/inline-svgs/replay-demo-icon.svg';
 import TakeControlIcon from '../images/inline-svgs/take-control-icon.svg';
+import RowsIcon from '../images/inline-svgs/rows.svg'
+import ColumnsIcon from '../images/inline-svgs/columns.svg'
+import IntegratedChartsIcon from '../images/inline-svgs/integrated-chart.svg'
 import styles from './Icon.module.scss';
 
 // Uses IBM Carbon Design System icons as a base
@@ -19,27 +22,28 @@ const SOCIALS_ICON_MAP = {
 
 const DOCS_CATEGORIES_ICON_MAP = {
     'docs-api': CarbonIcon.Api,
-    'docs-columns': CarbonIcon.Column,
-    'docs-row': CarbonIcon.Row,
-    'docs-tooling': CarbonIcon.BuildTool,
+    'docs-columns': ColumnsIcon,
+    'docs-row': RowsIcon,
+    'docs-tooling': CarbonIcon.ToolKit,
     'docs-styling': CarbonIcon.ColorPalette,
-    'docs-csd': CarbonIcon.CloudOffline,
+    'docs-csd': CarbonIcon.Gui,
     'docs-ssd': CarbonIcon.Db2Database,
     'docs-selection': CarbonIcon.CheckboxChecked,
     'docs-filtering': CarbonIcon.Filter,
-    'docs-rendering': CarbonIcon.Development,
+    'docs-rendering': CarbonIcon.DataVis_4,
     'docs-editing': CarbonIcon.WatsonHealthTextAnnotationToggle,
     'docs-group': CarbonIcon.CrossTab,
     'docs-detail': CarbonIcon.ShrinkScreen,
-    'docs-import-export': CarbonIcon.ImportExport,
+    'docs-import-export': CarbonIcon.Launch,
     'docs-accessories': CarbonIcon.ListDropdown,
     'docs-components': CarbonIcon.Settings,
     'docs-sparklines': CarbonIcon.Growth,
-    'docs-integrated-charts': CarbonIcon.ChartColumn,
+    'docs-integrated-charts': IntegratedChartsIcon,
     'docs-standalone-charts': CarbonIcon.SkillLevel,
-    'docs-scrolling': CarbonIcon.PanVertical,
-    'docs-interactivity': CarbonIcon.Touch_1,
-    'docs-testing': CarbonIcon.Report,
+    'docs-scrolling': CarbonIcon.FitToHeight,
+    'docs-interactivity': CarbonIcon.TouchInteraction,
+    'docs-testing': CarbonIcon.Task,
+    'docs-misc': CarbonIcon.IbmCloudEventNotification
 };
 
 const HOMEPAGE_FEATURES_ICON_MAP = {
