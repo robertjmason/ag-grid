28184443b1da30ecde593a89fe876c375b755589
Mark Durrant <mark.j.durrant@gmail.com>
2023-04-27 10:48:28 +0100

    AG-8094 - Refactor tabbed section markup & styles

--- grid-packages/ag-grid-docs/documentation/src/components/style-guide/containers.tsx
+++ grid-packages/ag-grid-docs/documentation/src/components/style-guide/containers.tsx
@@ -47,51 +47,87 @@ export const Containers = () => {
             </p>
 
             <div className="tabs-outer">
-                <header>
+                <header className="tabs-header">
                     <h3>Here's some tabs, you can switch content!</h3>
-                    <ul className="nav nav-tabs" id="myTab" role="tablist">
-                        <li className="nav-item" role="presentation">
+                    <ul className="nav nav-tabs tabs-nav-list" id="myTab" role="tablist">
+                        <li className="nav-item tabs-nav-item" role="presentation">
                             <a
-                                className="nav-link active"
+                                className="nav-link tabs-nav-link active"
                                 data-toggle="tab"
-                                href="#component"
+                                href="#sec-one"
                                 role="tab"
-                                aria-controls="component"
+                                aria-controls="sec-one"
                                 aria-selected="true"
                             >
-                                Component
+                                Section ONE
                             </a>
                         </li>
-                        <li className="nav-item" role="presentation">
+                        <li className="nav-item tabs-nav-item" role="presentation">
                             <a
-                                className="nav-link"
-                                id="template-tab"
+                                className="nav-link tabs-nav-link"
+                                id="sec-two-tab"
                                 data-toggle="tab"
-                                href="#template"
+                                href="#sec-two"
                                 role="tab"
-                                aria-controls="template"
+                                aria-controls="sec-two"
                                 aria-selected="false"
                             >
-                                Template
+                                Section TWO
+                            </a>
+                        </li>
+                        <li className="nav-item tabs-nav-item" role="presentation">
+                            <a
+                                className="nav-link tabs-nav-link"
+                                id="sec-three-tab"
+                                data-toggle="tab"
+                                href="#sec-three"
+                                role="tab"
+                                aria-controls="sec-three"
+                                aria-selected="false"
+                            >
+                                Section THREE
+                            </a>
+                        </li>
+                        <li className="nav-item tabs-nav-item" role="presentation">
+                            <a
+                                className="nav-link tabs-nav-link"
+                                id="sec-four-tab"
+                                data-toggle="tab"
+                                href="#sec-four"
+                                role="tab"
+                                aria-controls="sec-four"
+                                aria-selected="false"
+                            >
+                                Section FOUR
                             </a>
                         </li>
                     </ul>
                 </header>
-                <div className="tab-content" id="myTabContent">
+                <div className="tab-content tabs-content" id="myTabContent">
                     <div
-                        id="component"
+                        id="sec-one"
                         role="tabpanel"
-                        aria-labelledby="component-tab"
-                        className="tab-pane show active"
+                        aria-labelledby="sec-one-tab"
+                        className="tab-pane tabs-panel show active"
                     >
-                        Et inventore est veniam expedita adipisci. Dolor rerum in ex illo. Rerum autem deleniti aut
-                        eligendi tempora aliquam nihil id magnam. Porro eveniet quisquam voluptate labore tempore saepe
-                        qui qui facilis.
+                        <b>Section ONE:</b> Et inventore est veniam expedita adipisci. Dolor rerum in ex illo. Rerum
+                        autem deleniti aut eligendi tempora aliquam nihil id magnam. Porro eveniet quisquam voluptate
+                        labore tempore saepe qui qui facilis.
                     </div>
-                    <div id="template" role="tabpanel" aria-labelledby="template-tab" className="tab-pane">
-                        Ut natus velit quaerat quas quis distinctio illo aut. Neque autem atque sunt doloribus illum
-                        fuga quam est mollitia. Et molestiae quia vero quos ipsa est eius voluptates repellendus.
-                        Placeat consequatur maiores provident.
+                    <div id="sec-two" role="tabpanel" aria-labelledby="sec-two-tab" className="tab-pane tabs-panel">
+                        <b>Section TWO:</b> Ut natus velit quaerat quas quis distinctio illo aut. Neque autem atque sunt
+                        doloribus illum fuga quam est mollitia. Et molestiae quia vero quos ipsa est eius voluptates
+                        repellendus. Placeat consequatur maiores provident.
+                    </div>
+                    <div id="sec-three" role="tabpanel" aria-labelledby="sec-three-tab" className="tab-pane tabs-panel">
+                        <b>Section THREE:</b> Velit laboriosam sed numquam excepturi quam distinctio incidunt ut ut.
+                        Odit in quia nemo officiis perferendis aspernatur animi molestiae. Quia recusandae dolorem hic
+                        repellat.
+                    </div>
+                    <div id="sec-four" role="tabpanel" aria-labelledby="sec-four-tab" className="tab-pane tabs-panel">
+                        <b>Section FOUR:</b> Harum non non eveniet quo sit. Et qui aut cum. Aliquid sunt magnam quidem
+                        qui non sint qui. Quis ex voluptatibus ad id eaque dolor tempora qui est. Vitae velit cum sunt
+                        eaque illum quae et. Quo sapiente at est vitae ipsam.
                     </div>
                 </div>
             </div>
